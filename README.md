e12e6bc54de052c7071d9bd6387b176eae4949fa
Miguel González de Chaves <miguelangel.gonzalez.dechaves@bbvaitspain.com>
2023-09-06 12:06:29 +0200

    [feat] PDF Mobile viewer

---
+++ README.md
@@ -0,0 +1,35 @@
+# PDF Mobile Viewer
+
+## Información
+
+La aplicación recibe los siguientes parámetros de entrada por query-params:
+* `pdf` La URL absoluta del pdf a mostrar en el visor
+* `lang` Idioma. Acepta el español (`es`) e inglés (`en`). Si no se le informa lo coge del navegador.
+* `iframe`. Boolean, por defecto toma el valor `false`. Si se informa a `true` informará errores y acciones de descarga vía postMessage a `window.top`. En caso contrario, la descarga se hará con un `window.top.open()`. Los eventos son:
+    * `loaded`
+    * `download`
+    * `error`
+
+## Install
+
+```
+npm install
+```
+
+## Build
+
+This project has been created using **webpack-cli**, you can now run
+
+```
+npm run build
+```
+
+or
+
+```
+yarn build
+```
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const stylesHandler = 'style-loader';
+
+
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+        static: {
+            directory: path.join(__dirname, 'assets'),
+        },
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'assets', to: 'assets' },
+                { from: 'node_modules/pdfjs-dist/web/pdf_viewer.css', to: 'assets/pdf_viewer.css' },
+                { from: 'node_modules/pdfjs-dist/build/pdf.js', to: 'assets/pdf.js' },
+                { from: 'node_modules/pdfjs-dist/web/pdf_viewer.js', to: 'assets/pdf_viewer.js' },
+                { from: 'node_modules/pdfjs-dist/build/pdf.worker.js', to: 'assets/pdf.worker.js' },
+                { from: 'node_modules/pdfjs-dist/cmaps', to: 'assets/cmaps' },
+                // { from: 'node_modules/pdfjs-dist/', to: 'assets/pdfjs-dist' },
+            ]
+
+            // http://localhost:9000/assets/pdfjs-dist/build/pdf.worker.js
+        })
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler, 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+    optimization: {
+        minimizer: isProduction ? [
+          new CssMinimizerPlugin(),
+        ] : [],
+      },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
